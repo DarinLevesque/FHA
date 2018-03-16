68d1839bf899f27d0fe529232b208f5e97c653fb
Darin Levesque <darin@darinlevesque.com>
2018-03-16 13:07:36 -0400

    add simple checkout form

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -3,64 +3,6 @@ import CardReactFormContainer from 'card-react';
 
 import './checkout.css';
 
-// Create a Stripe client.
-const stripe = Stripe('pk_test_bwDxe6R8crYZebNVmjYu9Dxr');
-
-// Create an instance of Elements.
-const elements = stripe.elements();
-
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
-const style = {
-  base: {
-    color: '#32325d',
-    lineHeight: '18px',
-    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-    fontSmoothing: 'antialiased',
-    fontSize: '16px',
-    '::placeholder': {
-      color: '#aab7c4'
-    }
-  },
-  invalid: {
-    color: '#fa755a',
-    iconColor: '#fa755a'
-  }
-};
-
-// Create an instance of the card Element.
-const card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>.
-card.mount('#card-element');
-
-// Handle real-time validation errors from the card Element.
-card.addEventListener('change', function(event) {
-  const displayError = document.getElementById('card-errors');
-  if (event.error) {
-    displayError.textContent = event.error.message;
-  } else {
-    displayError.textContent = '';
-  }
-});
-
-// Handle form submission.
-const form = document.getElementById('payment-form');
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  stripe.createToken(card).then(function(result) {
-    if (result.error) {
-      // Inform the user if there was an error.
-      const errorElement = document.getElementById('card-errors');
-      errorElement.textContent = result.error.message;
-    } else {
-      // Send the token to your server.
-      stripeTokenHandler(result.token);
-    }
-  });
-});
-
 export default class Checkout extends React.Component {
   render () {
     return (
@@ -135,6 +77,17 @@ export default class Checkout extends React.Component {
                       </form>
 
                     </CardReactFormContainer>
+                    <form action="your-server-side-code" method="POST">
+                      <script
+                        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                        data-key="pk_test_bwDxe6R8crYZebNVmjYu9Dxr"
+                        data-amount="999"
+                        data-name="Fair Housing Advocates, Inc."
+                        data-description="Example charge"
+                        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
+                        data-locale="auto">
+                      </script>
+                    </form>
                   </div>
                 </div>
               </div>
