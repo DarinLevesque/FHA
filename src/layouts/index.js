2dc9373fc4a7372a00b137cc4ba710ca6545d0d9
Darin Levesque <darinlevesque@gmail.com>
2018-03-19 14:05:54 -0400

    removed /product/ added react-bootstrap

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -9,6 +9,11 @@ const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Fair Housing Advocates, Inc.">
     <script src="https://js.stripe.com/v3/" type="text/javascript" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     </ Helmet>
     <Navbar />
     <div>{children()}</div>
