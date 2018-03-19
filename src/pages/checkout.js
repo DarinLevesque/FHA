3e1881e6077bc66f31986195e937ef9191ed3721
Darin Levesque <darinlevesque@gmail.com>
2018-03-19 13:57:46 -0400

    removed /product/ added react-bootstrap

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -16,18 +16,6 @@ export default class Checkout extends React.Component {
                   Testing
                   </h2>
                   <div className="content">
-                    <form action="your-server-side-code" method="POST">
-                      <script
-                        src="https://checkout.stripe.com/checkout.js" className="stripe-button"
-                        data-key="pk_test_bwDxe6R8crYZebNVmjYu9Dxr"
-                        data-amount="12.99"
-                        data-zip-code="true"
-                        data-name="Fair Housing Advocates, Inc."
-                        data-description="Widget"
-                        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                        data-locale="auto">
-                      </script>
-                    </form>
                     <div id="card-wrapper"></div>
                     <CardReactFormContainer
 
@@ -79,13 +67,14 @@ export default class Checkout extends React.Component {
                     </CardReactFormContainer>
                     <form action="your-server-side-code" method="POST">
                       <script
-                        src="https://checkout.stripe.com/checkout.js" class="stripe-button"
+                        src="https://checkout.stripe.com/checkout.js" className="stripe-button"
                         data-key="pk_test_bwDxe6R8crYZebNVmjYu9Dxr"
                         data-amount="999"
                         data-name="Fair Housing Advocates, Inc."
-                        data-description="Example charge"
+                        data-description="Widget"
                         data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                        data-locale="auto">
+                        data-locale="auto"
+                        data-zip-code="true">
                       </script>
                     </form>
                   </div>
