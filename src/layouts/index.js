0a5051353e3db4087262ebeb89f2110843e39fb6
Darin Levesque <darinlevesque@gmail.com>
2018-03-19 15:58:27 -0400

    possible payment form fix testing with https

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,13 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import Navbar from '../components/Navbar';
+import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Fair Housing Advocates, Inc.">
-    <script src="https://js.stripe.com/v3/" type="text/javascript" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" />
     </ Helmet>
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,86 +4,81 @@ import CardReactFormContainer from 'card-react';
 import './checkout.css';
 
 export default class Checkout extends React.Component {
-  render () {
+  constructor(props) {
+    super(props);
+    this.state = {
+        loading: true,
+        stripeLoading: true,
+    };
+    // onStripeUpdate must be bound or else clicking on button will produce error.
+    this.onStripeUpdate = this.onStripeUpdate.bind(this);
+    // binding loadStripe as a best practice, not doing so does not seem to cause error.
+    this.loadStripe = this.loadStripe.bind(this);
+}
+
+loadStripe(onload) {
+    if(! window.StripeCheckout) {
+        const script = document.createElement('script');
+        script.onload = function () {
+            console.info("Stripe script loaded");
+            onload();
+        };
+        script.src = 'https://checkout.stripe.com/checkout.js';
+        document.head.appendChild(script);
+    } else {
+        onload();
+    }
+}
+
+componentDidMount() {
+
+    this.loadStripe(() => {
+        this.stripeHandler = window.StripeCheckout.configure({
+            key: 'pk_test_bwDxe6R8crYZebNVmjYu9Dxr',
+            image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+            locale: 'auto',
+            token: (token) => {
+                this.setState({ loading: true });
+                // use fetch or some other AJAX library here if you dont want to use axios
+                axios.post('/your-server-side-code', {
+                    stripeToken: token.id,
+                });
+            }
+        });
+
+        this.setState({
+            stripeLoading: false,
+            // loading needs to be explicitly set false so component will render in 'loaded' state.
+            loading: false,
+        });
+    });
+}
+
+componentWillUnmount() {
+    if(this.stripeHandler) {
+        this.stripeHandler.close();
+    }
+}
+
+onStripeUpdate(e) {
+    this.stripeHandler.open({
+        name: 'test',
+        description: 'widget',
+        panelLabel: 'Update Credit Card',
+        allowRememberMe: false,
+    });
+    e.preventDefault();
+}
+
+render() {
+    const { stripeLoading, loading } = this.state;
     return (
-      <div>
-        <section className="section section--gradient">
-          <div className="container">
-            <div className="columns">
-              <div className="column is-10 is-offset-1">
-                <div className="section">
-                  <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
-                  Testing
-                  </h2>
-                  <div className="content">
-                    <div id="card-wrapper"></div>
-                    <CardReactFormContainer
-
-                      // the id of the container element where you want to render the card element.
-                      // the card component can be rendered anywhere (doesn't have to be in ReactCardFormContainer).
-                      container="card-wrapper" // required
-
-                      // an object contain the form inputs names.
-                      // every input must have a unique name prop.
-                      formInputsNames={
-                        {
-                          number: 'CCnumber', // optional — default "number"
-                          expiry: 'CCexpiry',// optional — default "expiry"
-                          cvc: 'CCcvc', // optional — default "cvc"
-                          name: 'CCname' // optional - default "name"
-                        }
-                      }
-
-                      // initial values to render in the card element
-                      initialValues= {
-                        {
-                          number: '•••• •••• •••• ••••', // optional — default •••• •••• •••• ••••
-                          cvc: '123', // optional — default •••
-                          expiry: '16/12', // optional — default ••/••
-                          name: 'Nancy Smith' // optional — default FULL NAME
-                        }
-                      }
-
-                      // the class name attribute to add to the input field and the corresponding part of the card element,
-                      // when the input is valid/invalid.
-                      classes={
-                        {
-                          valid: 'valid-input', // optional — default 'jp-card-valid'
-                          invalid: 'invalid-input' // optional — default 'jp-card-invalid'
-                        }
-                      }
-
-                      // specify whether you want to format the form inputs or not
-                      formatting={true} // optional - default true
-                    >
-
-                      <form>
-                        <input placeholder="Full name" type="text" name="CCname" />
-                        <input placeholder="Card number" type="text" name="CCnumber" />
-                        <input placeholder="MM/YY" type="text" name="CCexpiry" />
-                        <input placeholder="CVC" type="text" name="CCcvc" />
-                      </form>
-
-                    </CardReactFormContainer>
-                    <form action="your-server-side-code" method="POST">
-                      <script
-                        src="https://checkout.stripe.com/checkout.js" className="stripe-button"
-                        data-key="pk_test_bwDxe6R8crYZebNVmjYu9Dxr"
-                        data-amount="999"
-                        data-name="Fair Housing Advocates, Inc."
-                        data-description="Widget"
-                        data-image="https://stripe.com/img/documentation/checkout/marketplace.png"
-                        data-locale="auto"
-                        data-zip-code="true">
-                      </script>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </div>  
-    )
-  }
+        <div>
+            {(loading || stripeLoading)
+                ? <p>loading..</p>
+                : <button onClick={this.onStripeUpdate}>Add CC</button>
+            }
+        </div>
+    );
+}
 }
