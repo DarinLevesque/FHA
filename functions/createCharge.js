7d11d1551c9388fed1c59cc858821510c355739c
Darin Levesque <darinlevesque@gmail.com>
2018-03-21 23:20:58 -0400

    testing

--- functions/createCharge.js
+++ functions/createCharge.js
@@ -1,4 +1,4 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(pk_test_bwDxe6R8crYZebNVmjYu9Dxr);
 
 exports.handler = function(event, context, callback) {
   const requestBody = JSON.parse(event.body);
