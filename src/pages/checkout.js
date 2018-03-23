0ac82f2d8674b26e3d3bd9a439f88145a27a55d5
Darin Levesque <darinlevesque@gmail.com>
2018-03-23 00:18:50 -0400

    stripe testing

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,65 +1,80 @@
 import React from 'react';
 import CardReactFormContainer from 'card-react';
+import uuid from 'uuid';
 
 import './checkout.css';
 
-const amount = 1000;
-const $messageBox = document.getElementById('messageBox');
-const $button = document.querySelector('button');
-
 export default class Checkout extends React.Component {
-    function resetButtonText() {
-      $button.innerHTML = 'Click to Buy! <strong>$10</strong>';
+  constructor(props) {
+    super(props);
+    this.state = {
+        loading: true,
+        stripeLoading: true,
+    };
+    // onStripeUpdate must be bound or else clicking on button will produce error.
+    this.onStripeUpdate = this.onStripeUpdate.bind(this);
+    // binding loadStripe as a best practice, not doing so does not seem to cause error.
+    this.loadStripe = this.loadStripe.bind(this);
+}
+
+loadStripe(onload) {
+    if(! window.StripeCheckout) {
+        const script = document.createElement('script');
+        script.onload = function () {
+            console.info("Stripe script loaded");
+            onload();
+        };
+        script.src = 'https://checkout.stripe.com/checkout.js';
+        document.head.appendChild(script);
+    } else {
+        onload();
     }
-    
-    const handler = StripeCheckout.configure({
-      key: STRIPE_PUBLISHABLE_KEY,
-      image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
-      locale: 'auto',
-      closed: function () {
-        resetButtonText();
-      },
-      token: function(token) {
-    
-        fetch(`${LAMBDA_ENDPOINT}purchase`, {
-          method: 'POST',
-          body: JSON.stringify({
-            token,
-            amount,
-            idempotency_key: uuid()
-          }),
-          headers: new Headers({
-            'Content-Type': 'application/json'
-          })
-        })
-        .then(res => res.json())
-        .catch(error => console.error(error))
-        .then(response => {
-    
-          resetButtonText();
-    
-          let message = typeof response === 'object' && response.status === 'succeeded'
-            ? 'Charge was successful!'
-            : 'Charge failed.'
-          $messageBox.querySelector('h2').innerHTML = message;
-    
-          console.log(response);
+}
+
+componentDidMount() {
+    this.loadStripe(() => {
+        this.stripeHandler = window.StripeCheckout.configure({
+            key: 'pk_test_bwDxe6R8crYZebNVmjYu9Dxr',
+            image: 'https://stripe.com/img/documentation/checkout/marketplace.png',
+            locale: 'auto',
+            token: (token) => {
+                this.setState({ loading: true });
+                // use fetch or some other AJAX library here if you dont want to use axios
+                fetch(`${LAMBDA_ENDPOINT}purchase`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                      token.id,
+                      amount,
+                      idempotency_key: uuid()
+                    }),
+                    headers: new Headers({
+                      'Content-Type': 'application/json'
+                    }), 
+                }
+        )}});
+
+        this.setState({
+            stripeLoading: false,
+            // loading needs to be explicitly set false so component will render in 'loaded' state.
+            loading: false,
         });
-      }
     });
-    
-    $button.addEventListener('click', () => {
-    
-      setTimeout(() => {
-        $button.innerHTML = 'Waiting for response...';
-      }, 500);
-    
-      handler.open({
-        amount,
-        name: 'Test Shop',
-        description: 'A Fantastic New Widget'
-      });
+}
+
+componentWillUnmount() {
+    if(this.stripeHandler) {
+        this.stripeHandler.close();
+    }
+}
+
+onStripeUpdate(e) {
+    this.stripeHandler.open({
+        name: 'test',
+        description: 'widget',
+        panelLabel: 'Update Credit Card',
+        allowRememberMe: false,
     });
+    e.preventDefault();
 }
 
 render() {
