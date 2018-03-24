5851ac0434d047875ae32536b9706ae887fa16d3
Darin Levesque <darinlevesque@gmail.com>
2018-03-24 17:52:28 -0400

    stripe testing

--- functions/createCharge.js
+++ functions/createCharge.js
@@ -1,40 +1,10 @@
-const stripe = require('stripe')(pk_test_bwDxe6R8crYZebNVmjYu9Dxr);
-
 exports.handler = function(event, context, callback) {
-  const requestBody = JSON.parse(event.body);
-  const token = requestBody.token.id;
-  const amount = requestBody.charge.amount;
-  const currency = requestBody.charge.currency;
+    var stripe = require("stripe")("sk_test_eSGpVDAcRjBrUoKVlvhsyNAs");
 
-  return stripe.charges.create({ // Create Stripe charge with token
-    amount,
-    currency,
-    description: 'Netlify Stripe Test charge',
-    source: token,
-  })
-    .then((charge) => { // Success response
-      const response = {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          message: `Charge processed succesfully!`,
-          charge,
-        }),
-      };
-      callback(null, response);
-    })
-    .catch((err) => { // Error response
-      const response = {
-        statusCode: 500,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-        body: JSON.stringify({
-          error: err.message,
-        }),
-      };
-      callback(null, response);
-    })
-};
+    const charge = stripe.charges.create({
+    amount: 999,
+    currency: 'usd',
+    source: 'tok_visa',
+    receipt_email: 'jenny.rosen@example.com',
+    });
+}
