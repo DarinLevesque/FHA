b629aee0e1b069a71bb849d1580c465e4e4045e5
Darin Levesque <darinlevesque@gmail.com>
2018-04-16 23:12:49 -0400

    reverted netlify-cms back to 1.0.9

---
+++ src/templates/property-services-page.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Link from "gatsby-link";
+import Content, { HTMLContent } from "../components/Content";
+
+export const PropServicesPageTemplate = ({
+  title,
+  content,
+  contentComponent
+}) => {
+  const PageContent = contentComponent || Content;
+
+  return (
+    <section className="section section--gradient">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <h1 className="page-header">
+              Property Services<br />
+              <small>Our Mission & Purpose</small>
+            </h1>
+            <nav class="breadcrumb" aria-label="breadcrumbs">
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li class="is-active">
+                  <Link to="/property-services" aria-current="page">
+                    PropServices
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="section">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+                {title}
+              </h2>
+              <PageContent className="content" content={content} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ({ data }) => {
+  const { markdownRemark: post } = data;
+
+  return (
+    <PropServicesPageTemplate
+      contentComponent={HTMLContent}
+      title={post.frontmatter.title}
+      content={post.html}
+    />
+  );
+};
+
+export const propservicesPageQuery = graphql`
+  query PropServicesPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
