e6152305896a8203916a2853c8f4a58f59008f35
Darin Levesque <darinlevesque@gmail.com>
2018-04-25 08:47:57 -0400

    removed document reference

--- src/templates/property-services-page.js
+++ src/templates/property-services-page.js
@@ -8,15 +8,6 @@ export const PropServicesPageTemplate = ({
   contentComponent
 }) => {
   const PageContent = contentComponent || Content;
-  var modal = document.getElementById("modal");
-  var elements = document.getElementsByClassName("toggle-modal");
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].addEventListener("click", toggleClass);
-  }
-
-  function toggleClass() {
-    modal.classList.toggle("is-active");
-  }
 
   return (
     <section className="section section--gradient">
