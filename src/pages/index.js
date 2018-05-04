d499a398a8b91a852857a55b4ecddfb94c4ffa18
Darin Levesque <darinlevesque@gmail.com>
2018-05-03 23:58:14 -0400

    added in typeform, and fixed background text

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,6 +13,26 @@ export default class IndexPage extends React.Component {
     const { data } = this.props;
     const { edges: posts } = data.allMarkdownRemark;
 
+    (function() {
+      var qs,
+        js,
+        q,
+        s,
+        d = document,
+        gi = d.getElementById,
+        ce = d.createElement,
+        gt = d.getElementsByTagName,
+        id = "typef_orm_share",
+        b = "https://embed.typeform.com/";
+      if (!gi.call(d, id)) {
+        js = ce.call(d, "script");
+        js.id = id;
+        js.src = b + "embed.js";
+        q = gt.call(d, "script")[0];
+        q.parentNode.insertBefore(js, q);
+      }
+    })();
+
     return (
       <section className="section">
         <div className="container">
@@ -22,43 +42,56 @@ export default class IndexPage extends React.Component {
           <div className="tile is-ancestor">
             <div className="tile is-4 is-vertical is-parent">
               <div className="tile is-child box">
-                <p className="title">Fair Housing Act Details</p>
+                <p className="title">Get Qualified!</p>
                 <p>
-                  The Fair Housing Act was enacted as Title VIII of the Civil
-                  Rights Act of 1968. It protects people from discrimination
-                  based on the protected classes established.
+                  <a
+                    className="typeform-share button is-link"
+                    href="https://kdventures.typeform.com/to/sJm9ls"
+                    data-mode="popup"
+                    style={{
+                      display: "inline-block",
+                      textDecoration: "none",
+                      backgroundColor: "#267DDD",
+                      color: "white",
+                      cursor: "pointer",
+                      fontFamily: "Helvetica,Arial,sans-serif",
+                      fontSize: 20,
+                      lineHeight: 50,
+                      textAlign: "center",
+                      margin: 0,
+                      height: 50,
+                      padding: "0px 33px",
+                      borderRadius: 1,
+                      maxWidth: "100%",
+                      whiteSpace: "nowrap",
+                      overflow: "hidden",
+                      textOverflow: "ellipsis",
+                      fontWeight: "bold",
+                      WebkitFontSmoothing: "antialiased",
+                      MozOsxFontSmoothing: "grayscale"
+                    }}
+                    target="_blank"
+                  >
+                    Qualify!
+                  </a>
                 </p>
               </div>
               <div className="tile is-child box">
                 <p className="title">Webinar Schedule</p>
-                <p>Content</p>
+                <p>Coming soon...</p>
               </div>
             </div>
             <div className="tile is-parent">
               <div className="tile is-child box">
-                <p className="title">Three</p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
-                  semper diam at erat pulvinar, at pulvinar felis blandit.
-                  Vestibulum volutpat tellus diam, consequat gravida libero
-                  rhoncus ut. Morbi maximus, leo sit amet vehicula eleifend,
-                  nunc dui porta orci, quis semper odio felis ut quam.
+                <p className="title">Our background</p>
+                <p style={{ marginTop: 10 }}>
+                  The Fair Housing Act was enacted as Title VIII of the Civil
+                  Rights Act of 1968. It protects people from discrimination
+                  based on the classes established in the law.
                 </p>
-                <p>
-                  Suspendisse varius ligula in molestie lacinia. Maecenas varius
-                  eget ligula a sagittis. Pellentesque interdum, nisl nec
-                  interdum maximus, augue diam porttitor lorem, et sollicitudin
-                  felis neque sit amet erat. Maecenas imperdiet felis nisi,
-                  fringilla luctus felis hendrerit sit amet. Aenean vitae
-                  gravida diam, finibus dignissim turpis. Sed eget varius
-                  ligula, at volutpat tortor.
-                </p>
-                <p>
-                  Integer sollicitudin, tortor a mattis commodo, velit urna
-                  rhoncus erat, vitae congue lectus dolor consequat libero.
-                  Donec leo ligula, maximus et pellentesque sed, gravida a
-                  metus. Cras ullamcorper a nunc ac porta. Aliquam ut aliquet
-                  lacus, quis faucibus libero. Quisque non semper leo.
+                <p style={{ marginTop: 10 }}>
+                  Fair Housing Advocates Inc. are experts in the Fair Housing
+                  Act claims process and education. We provide education seminars/webinars, property testing and other services which are all explained in our Property Pro section.
                 </p>
               </div>
             </div>
